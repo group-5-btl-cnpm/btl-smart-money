4d2bf82f3f65b665ac3a232d36ba18d73271cf04
Phạm Ngọc Duy <bboyphamduy1@gmail.com>
2022-05-15 00:39:06 +0700

    upload file

--- process/customer/process_id_group.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(isset($_POST['group_id'])) {
-    $_SESSION['group_id'] = $_POST['group_id'];
-    echo $_SESSION['group_id'];
-}
-?>
--- process/customer/process_id_spend.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['spend_id'])) {
-    $_SESSION["spend_id"] = $_POST['spend_id'];
-    echo $_SESSION["spend_id"];
-}
